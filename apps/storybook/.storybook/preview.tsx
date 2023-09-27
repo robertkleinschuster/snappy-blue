899016000e2bafa3598bbccb98f63f96c7add15f
robertkleinschuster <git@robs.social>
2023-09-27 16:53:24 +0200

    setup storybook

---
+++ apps/storybook/.storybook/preview.tsx
@@ -0,0 +1,24 @@
+import { Preview } from '@storybook/react';
+import {NextUIProvider} from "@nextui-org/react";
+import "../src/index.css"
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+        <NextUIProvider>
+          <Story />
+        </NextUIProvider>
+    ),
+  ],
+};
+
+export default preview;
---
+++ apps/storybook/tailwind.config.ts
@@ -0,0 +1,17 @@
+import {nextui} from "@nextui-org/react";
+
+import type {Config} from 'tailwindcss'
+
+export default {
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx}",
+        '../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {},
+    },
+    darkMode: 'class',
+    plugins: [nextui({
+        defaultTheme: 'dark'
+    })],
+} satisfies Config
