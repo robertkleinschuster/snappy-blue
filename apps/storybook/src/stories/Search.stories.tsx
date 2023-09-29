86b190accffe8f62789860876e747ba6a5ab8232
robertkleinschuster <git@robs.social>
2023-09-29 20:57:47 +0200

    search input component - adjust storybook layout

--- apps/storybook/src/stories/Search.stories.tsx
+++ apps/storybook/src/stories/Search.stories.tsx
@@ -17,7 +17,7 @@ export default {
   component: Search,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
-    layout: "centered",
+    layout: "padded",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ["autodocs"],
