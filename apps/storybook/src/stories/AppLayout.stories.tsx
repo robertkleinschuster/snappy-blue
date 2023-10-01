3b99366ffbe58b07f8befb5ebd6e635e635f4493
robertkleinschuster <git@robs.social>
2023-10-01 12:58:25 +0200

    added app layout stories

---
+++ apps/storybook/src/stories/AppLayout.stories.tsx
@@ -0,0 +1,69 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Tabs, Tab } from "@nextui-org/react";
+import { Outlet, RouterProvider, createMemoryRouter, useNavigate } from "react-router-dom";
+
+const router = createMemoryRouter(
+  [
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          path: "favorites",
+          element: <Favorites/>,
+        },
+        {
+          path: "trains",
+          element: <Trains/>,
+        },
+        {
+          path: "departures",
+          element: <Departures/>,
+        },
+      ],
+    },
+  ],
+  {}
+);
+
+function Favorites(): React.JSX.Element {
+  return <>Hello favorites!</>
+}
+
+function Trains(): React.JSX.Element {
+  return <>Hello trains!</>
+}
+
+function Departures(): React.JSX.Element {
+  return <>Hello departures!</>
+}
+
+function AppLayout() {
+  const navigate = useNavigate()
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex-grow">
+        <Outlet />
+      </div>
+      <Tabs onSelectionChange={(key) => navigate(key as string)} fullWidth radius="none" size="lg">
+        <Tab key="favorites" title="Favorites"></Tab>
+        <Tab key="trains" title="Trains"></Tab>
+        <Tab key="departures" title="Departures"></Tab>
+      </Tabs>
+    </div>
+  );
+}
+
+function App(): React.JSX.Element {
+  return <RouterProvider router={router}/>
+}
+
+export default {
+  title: "AppLayout",
+  component: App,
+  parameters: {
+    layout: 'fullscreen'
+  },
+} satisfies Meta;
+
+export const Primary: StoryObj = {};
