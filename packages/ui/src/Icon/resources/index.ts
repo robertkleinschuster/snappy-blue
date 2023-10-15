13a8d9d5e7894fc3fda403a549f8c63bea88b46a
kevinwalitsch <github@walitsch.at>
2023-10-15 18:31:57 +0200

    added db 4-6 fonts

--- packages/ui/src/Icon/resources/index.ts
+++ packages/ui/src/Icon/resources/index.ts
@@ -10,6 +10,9 @@ export const ICONS_OEBB = oebbResource.fontFamily;
 export const ICONS_DB_1 = db.resource1.fontFamily;
 export const ICONS_DB_2 = db.resource2.fontFamily;
 export const ICONS_DB_3 = db.resource3.fontFamily;
+export const ICONS_DB_4 = db.resource4.fontFamily;
+export const ICONS_DB_5 = db.resource5.fontFamily;
+export const ICONS_DB_6 = db.resource6.fontFamily;
 
 const resources = new ResourceCollection();
 
@@ -19,5 +22,8 @@ resources.add(oebbResource);
 resources.add(db.resource1);
 resources.add(db.resource2);
 resources.add(db.resource3);
+resources.add(db.resource4);
+resources.add(db.resource5);
+resources.add(db.resource6);
 
 export { resources as iconResources };
